b8398efe5e80ac84ea92a3016a558cd1e5128676
Lasse Nørfeldt <678806+Norfeldt@users.noreply.github.com>
2021-05-27 22:51:47 +0200

    Update index.d.ts
    This will allow auto-suggestion in vscode

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,10 @@
-/// <reference types="cypress" />
-
-// All Stripe Elements fields have a `data-elements-stable-field-name`
-// attribute that can be used to target it.
-//
-// This type is meant to hint you about the basic card input fields you can fill.
-//
-// In case we haven't added a stable field name to this list, you can always
-// fallback to passing a `string`.
-declare module "cypress-plugin-stripe-elements" {
-  export type CypressStripeElementsFieldName = 'cardCvc' | 'cardNumber' | 'cardExpiry' | 'postalCode' | string;
-}
-
 declare namespace Cypress {
+  // These type is meant to hint you about the basic card input fields you can fill.
+  type CypressStripeElementsFieldName = 'cardCvc' | 'cardNumber' | 'cardExpiry' | 'postalCode'
+    
   interface Chainable {
-    /**
-     * Custom command to select DOM element by data-cy attribute.
-     * @example cy.dataCy('greeting')
-    */
-    fillElementsInput(field: CypressStripeElementsFieldName, value: string): Chainable<Element>
+    fillElementsInput<T>(field: T extends string ? string : CypressStripeElementsFieldName, value: string): Chainable<Element>
   }
+  // In case we haven't added a stable field name, you can fix type warnings by
+  // cy.fillElementsInput<string>('extraField,'some value')
 }
